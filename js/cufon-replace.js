731dba5998db58d085c3c68b98a282b5f861a2d7
Алина Платонова <alina_platonova@bk.ru>
2016-02-01 20:12:55 +0300

    Initial commit

---
+++ js/cufon-replace.js
@@ -0,0 +1,3 @@
+Cufon.replace('.aplatonova', {fontFamily: 'apl', hover:true, ignoreClass:'nocufon', letterSpacing:'1px', fontSize:'16px'});
+Cufon.replace('li', {fontFamily: 'apl', hover:true, ignoreClass:'nocufon', letterSpacing:'0.5px', fontSize:'14px'});
+Cufon.replace('.latoB', {fontFamily: 'Lato_Black', hover:true, ignoreClass:'nocufon', color:'#22223e', fontSize:'24px'});
